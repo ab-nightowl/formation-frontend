0d22a52b123cee7118a56c6d6d7c0bc96984eed3
Alexandre BROCARD <ab.nightowl33@gmail.com>
2017-08-18 17:32:02 +0200

    promesses OK! Tp finished

--- 04-es2015/app.js
+++ 04-es2015/app.js
@@ -70,6 +70,8 @@ console.log(parisTrip.toString());
 const defaultTrip = Trip.getDefaultTrip();
 console.log(defaultTrip.toString());
 
+
+
 class FreeTrip extends Trip {
 	constructor(id, name, imageUrl, price = 0) {
 		super(id, name, imageUrl);
@@ -84,4 +86,102 @@ class FreeTrip extends Trip {
 
 const freeTrip = new FreeTrip('nantes', 'Nantes', 'img/nanges.jpg');
 console.log(freeTrip.toString());
+
+
+
+class TripService {
+	constructor() {
+		this.tripsSet = new Set(
+			[
+				new Trip('paris', 'Paris', 'img/paris.jpg'),
+				new Trip('nantes', 'Nantes', 'img/nanges.jpg'),
+				new Trip('rio-de-janeiro', 'Rio de Janeiro', 'img/rio-de-janeiro.jpg')
+			]);
+
+	}
+	
+	findByName(tripName) {
+	// TODO return promise
+		return new Promise( (resolve, reject) => {
+			setTimeout( () => {
+
+				this.tripsSet.forEach( (trip) => {
+					if (trip.name === tripName) {
+						resolve(trip);
+					}
+				});
+				reject(`No trip with name ${tripName}`);
+			}
+			, 2000);
+		});
+	}
+
+}
+
+
+class PriceService {
+	constructor() {
+	// TODO Map of 2 trips
+		this.pricesMap = new Map(
+			[
+				['paris', 100],
+				['rio-de-janeiro', 800]
+			]);
+	}
+	
+	findPriceByTripId(tripId) {
+	// TODO return promise
+		return new Promise( (resolve, reject) => {
+			setTimeout( () => {
+
+				if (this.pricesMap.has(tripId)) {
+					let price = this.pricesMap.get(tripId);
+					resolve(price);
+				} else {
+					reject(`No price found for id ${tripId}`);
+				}
+				
+			}
+			, 2000);
+		});
+	}
+}
+
+
+console.log("\n****** Promesses ******\n");
+
+const tripService = new TripService();
+const priceService = new PriceService();
+
+const prom1 = tripService.findByName('Paris');
+const prom2 = tripService.findByName('Toulouse');
+
+prom1.then( trip => {
+	console.log(`Trip found: Trip ${trip}`);
+	}, error => {
+		console.log(error);
+	}
+);
+prom2.then( trip => {
+	console.log(`Trip found: Trip ${trip}`);
+	}, error => {
+		console.log(error);
+	}
+);
+
+const prom3 = priceService.findPriceByTripId('rio-de-janeiro');
+const prom4 = priceService.findPriceByTripId('nantes');
+
+prom3.then( price => {
+	console.log(`Price found : ${price}`);
+	}, error => {
+		console.log(error);
+	}
+);
+prom4.then( price => {
+	console.log(`Price found : ${price}`);
+	}, error => {
+		console.log(error);
+	}
+);
 
